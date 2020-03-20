64557ca1a3f272f64c0c6712584714808438c1bd
agonzalezacevedo <56076787+agonzalezacevedo@users.noreply.github.com>
2020-03-20 16:16:34 +0100

    Final

--- src/singleton/Persona.java
+++ src/singleton/Persona.java
@@ -6,26 +6,53 @@ package singleton;
  */
 public class Persona {
 
-    String nome;
-    String apellido;
-    int edad;
+    private static String nome;
+    private static String apelido;
+    private static int edad;
     private static Persona launica;
     
     private Persona() {
     }
 
-    private Persona(String nome, String apellido, int edad) {
-        this.nome = nome;
-        this.apellido = apellido;
-        this.edad = edad;
+    private Persona(String nome, String apelido, int edad) {
+        launica.nome = nome;
+        launica.apelido = apelido;
+        launica.edad = edad;
     }
-    public static Persona getInstance(){
-        launica = new Persona();
+    public static Persona getInstance(String nome, String apelido, int edad){
+        if(launica==null){
+            launica = new Persona(nome,apelido,edad);
+        }
         return launica;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
     @Override
     public String toString() {
-        return "Persona{" + "nome=" + nome + ", apellido=" + apellido + ", edad=" + edad + '}';
+        return "Persona: " + "nome=" + nome + ", apellido=" + apelido + ", edad=" + edad;
     }
     
 }
--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -7,10 +7,11 @@ package singleton;
 public class Singleton {
 
     public static void main(String[] args) {
-        Persona per = Persona.getInstance();
-        Persona otra = Persona.getInstance();
-        per.toString();
-        otra.toString();
+        Persona per = Persona.getInstance("Gabriel","Campos",21);
+        System.out.println(per.toString());
+        Persona otra = Persona.getInstance("Xavier","García",17);
+        System.out.println(otra.toString());
+        
     }
     
 }
